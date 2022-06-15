8b968f5466b9cc2f51805b807a60c1c406eee211
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-06-15 14:27:16 +0200

    Update GGR.py

--- GGR.py
+++ GGR.py
@@ -1,11 +1,11 @@
-
-
 conda install -c anaconda pymongo
 import pymongo
 from pymongo import MongoClient
 import pprint
 
-client = MongoClient('mongodb://localhost:27017/')
+
+
+client = MongoClient('mongodb://localhost:27018/')
 ggr = client.ggr
 limerick_places = ggr.limerick.places
 
@@ -15,5 +15,8 @@ for post in limerick_places.find():
     pprint.pprint(post)
 
 # If we just want to know how many documents match a query we can perform a count_documents()
-limerick_places.count_documents({"name": "Limerick"})
+limerick_places.count_documents({"name": "Limerick"}) # 6
+limerick_places.count_documents({"country": "Irlanda"}) # 1314
+
+
 
