2cf4be46a481b28d2ee2811c46e39d232e6c2e6f
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-06-16 14:15:22 +0200

    Update GGR.py

--- GGR.py
+++ GGR.py
@@ -8,6 +8,7 @@ import pprint
 client = MongoClient('mongodb://localhost:27018/')
 ggr = client.ggr
 limerick_places = ggr.limerick.places
+limerick_posts = ggr.limerick.posts
 
 # To get more than a single document as the result of a query we use the find() method
 
@@ -18,5 +19,6 @@ for post in limerick_places.find():
 limerick_places.count_documents({"name": "Limerick"}) # 6
 limerick_places.count_documents({"country": "Irlanda"}) # 1314
 
-
-
+### Indexing
+# MongoDB provides a text index type that supports searching for string content in a collection.
+# Indexes can improve the efficiency of read operations.
