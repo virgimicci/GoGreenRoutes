eb7daa36147bf5f729067336e8dbe6be83820724
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-06-20 11:56:24 +0200

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,69 +1,58 @@
 
 conda install -c conda-forge spacy
-import spacy
 !python -m spacy download en_core_web_sm
+import spacy
 nlp = spacy.load('en_core_web_sm')
 import re #library for regular expressions
+import pandas as pd
 
-### Tokenization
-# The first step of preprocessing text data is to break every sentence into individual words, 
-# which is called tokenization.
-
-# Example for the first post
-text = df_lim_nat.iloc[0]["text"]
-# "Still time to see our stand at the Great Limerick Run Expo at 
-# the UL arena, Limerick.It's open until 6PM. #Neartonature #UL #Limerick"
-
-print([str(token) for token in nlp(text) if not token.is_punct]) # if not token.is_punct removed punctuations 
-
-# ['Still', 'time', 'to', 'see', 'our', 'stand', 'at', 'the', 'Great', 'Limerick', 'Run', 
-# 'Expo', 'at', 'the', 'UL', 'arena', 'Limerick', 'It', "'s", 'open', 'until', '6PM', 'Neartonature', 'UL', 'Limerick']
+# my df = df_lim_nat 2380
 
 ### Text cleaning 
+# It's important to follow this order
 
-# Tokenization and remove punctuations
-words = [str(token) for token in nlp(text) if not token.is_punct] 
+# Creation of a new text column called text1 with the first pre-processing step: lower case
+df_lim_nat['text1'] = df_lim_nat['text'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+df_lim_nat['text1'].head()
+
+# Remove hyperlinks
+df_lim_nat['text1'] = [re.sub(r'https?:\/\/.\S+', "", x) for x in df_lim_nat['text1']]
+
+# Remove websites and email address
+df_lim_nat['text1'] = [re.sub(r"\S+com", "", x) for x in df_lim_nat['text1']]
+df_lim_nat['text1'] = [re.sub(r"\S+@\S+", "", x) for x in df_lim_nat['text1']]
+
+# Remove old style retweet text "RT"
+df_lim_nat['text1'] = [re.sub(r'^RT[\s]+', '', x) for x in df_lim_nat['text1']]
 
 # Expanding Contractions
 # dictionary consisting of the contraction and the actual value
-Apos_dict = {"'s":" is","n't":" not","'m":" am","'ll":" will",
+apos_dict = {"'s":" is","n't":" not","'m":" am","'ll":" will",
            "'d":" would","'ve":" have","'re":" are"}
 # replace the contractions
-for key,value in Apos_dict.items():
-    if key in words:
-        words = words.replace(key,value)
-
-# Remove digits and other symbols except "@"--used to remove email
-words = [re.sub(r"[^A-Za-z@]", "", word) for word in words]
-
-# Remove websites and email address
-words = [re.sub(r"\S+com", "", word) for word in words]
-words = [re.sub(r"\S+@\S+", "", word) for word in words]
-
-# Remove hyperlinks
-words = [re.sub(r'https?:\/\/.\S+', "", word) for word in words]
-
-# Remove empty spaces 
-words = [word for word in words if word !=' ']
-
-# Remove old style retweet text "RT"
-words = [re.sub(r'^RT[\s]+', '', word) for word in words]
+for key,value in apos_dict.items():
+    if key in df['text1']:
+        df_lim_nat['text1'] = df_lim_nat['text1'].replace(key,value)
 
 # Split attached words
-words = " ".join([s for s in re.split("([A-Z][a-z]+[^A-Z]*)",words) if s])
+?????
+
+# Remove punctuations (anche hashtag)
+df_lim_nat['text1'] = df_lim_nat['text1'].str.replace('[^\w\s]','')
+df_lim_nat['text1'].head()
+
 
 ### Stopwords
 # The next step is to remove the useless words, namely, the stopwords. Stopwords are words that frequently appear in many articles,
 # but without significant meanings. Examples of stopwords are ‘I’, ‘the’, ‘a’, ‘of’.
-
 # spacy stopwords
 stopwords = nlp.Defaults.stop_words
-
 print(len(stopwords)) # 326
 print(stopwords)
 
 # exclude the stopwords from the text
-words = [word.lower() for word in words if word.lower() not in stopwords]
+df_lim_nat['text1'] = df_lim_nat['text1'].apply(lambda x: " ".join(x for x in x.split() if x not in stopwords))
+
 
 ### Lemmatization
 # to count the appearance of each word, it is essential to remove grammar tense and transform each word into its original form. 
@@ -74,7 +63,13 @@ words = [word.lower() for word in words if word.lower() not in stopwords]
 
 # .lemma_ function from spacy 
 
+def space(comment):
+    doc = nlp(comment)
+    return " ".join([token.lemma_ for token in doc])
+df_lim_nat['text1'] = df_lim_nat['text1'].apply(space)
+
 # Let's summarize the steps in a function and apply the function in all texts:
+
 
 def text_preprocessing(str_input): 
      #tokenization, remove punctuation, lemmatization
