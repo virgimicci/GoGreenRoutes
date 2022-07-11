6b314dfbcbc0398260ee746d7dcbf82abf976350
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-07-11 13:01:30 +0200

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,5 +1,7 @@
 # Using VADER lexicon for SA with nltk
-
+# VADER ( Valence Aware Dictionary for Sentiment Reasoning) is a model used for text sentiment analysis that is sensitive to both polarity (positive/negative) 
+# and intensity (strength) of emotion. 
+# It is available in the NLTK package and can be applied directly to unlabeled text data.
 
 
 import nltk
