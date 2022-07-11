318377ed1cce0d4c663faf9ed048b0245e5c558f
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-07-11 12:39:51 +0200

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -5,9 +5,25 @@
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid = SentimentIntensityAnalyzer()
+SIA = SentimentIntensityAnalyzer()
 
 
-# Example
+df_green['text1'] = df_green['text1'].astype(str)
 
-a = ''
+# Applying Model, Variable Creation
+df_green['Polarity Score'] = df_green['text1'].apply(lambda x:SIA.polarity_scores(x)['compound'])
+df_green['Neutral Score'] = df_green['text1'].apply(lambda x:SIA.polarity_scores(x)['neu'])
+df_green['Negative Score'] = df_green['text1'].apply(lambda x:SIA.polarity_scores(x)['neg'])
+df_green['Positive Score'] = df_green['text1'].apply(lambda x:SIA.polarity_scores(x)['pos'])
+
+# Converting 0 to 1 Decimal Score to a Categorical Variable
+df_green['Sentiment']=''
+df_green.loc[df_green['Polarity Score'] >0,'Sentiment']='Positive'
+df_green.loc[df_green['Polarity Score'] == 0,'Sentiment']='Neutral'
+df_green.loc[df_green['Polarity Score'] <0,'Sentiment']='Negative'
+df_green[:5]
+
+df_green["Sentiment"].value_counts()
+# Positive    626
+# Neutral     386
+# Negative     90
